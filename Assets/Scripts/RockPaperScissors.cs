a9753a82abe6f926196de8d1ff00f119fa040591
Smiley98 <smileysofficialcinema@gmail.com>
2025-02-19 10:50:05 -0500

    Added name submission

--- Assets/Scripts/RockPaperScissors.cs
+++ Assets/Scripts/RockPaperScissors.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,16 +6,33 @@ public class RockPaperScissors : MonoBehaviour
 {
     // SerializedField allows a value to be accessed within the Inspector (so we can drag & drop out button into this script)
     [SerializeField]
+    TMP_Text txtWelcome;
+
+    [SerializeField]
     Button btnRock;
+
+    [SerializeField]
+    TMP_InputField inpPlayerName;
+
+    // No need to store the player's name directly at the moment. Setting corresponding wigets on-input-submit
+    //string playerName = string.Empty;
 
     void Start()
     {
         // Tells Unity to run the OnClickHandler when we click our button
-        btnRock.onClick.AddListener(OnClickHandler);
+        btnRock.onClick.AddListener(OnRockClick);
+        inpPlayerName.onSubmit.AddListener(OnPlayerNameSubmit);
     }
 
-    void OnClickHandler()
+    void OnRockClick()
     {
         Debug.Log("You played Rock!");
+    }
+
+    void OnPlayerNameSubmit(string text)
+    {
+        string message = "Welcome, " + text;
+        txtWelcome.text = message;
+        Debug.Log(message);
     }
 }
