28ab4f0079357596815305d16357ae5201e17a54
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-19 10:23:52 -0400

    Added is-even function and comments

--- Assets/Scripts/Lab11.cs
+++ Assets/Scripts/Lab11.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class Lab11 : MonoBehaviour
 {
+    bool IsEven(int n)
+    {
+        // % means "modulus".
+        // Modulus calculates the remainder of a division.
+        // If we want to know whether a number is even, we can check if its divisible by 2!
+        return n % 2 == 0;
+    }
+
     void Start()
     {
-        int n = 5;
-        if (n % 2 == 0)
+        // Display even vs odd numbers 1-10
+        for (int i = 1; i <= 10; i++)
         {
-            Debug.Log("Even :)");
-        }
-        else
-        {
-            Debug.Log("Odd :(");
+            if (IsEven(i))
+            {
+                Debug.Log(i + " is even :)");
+            }
+            else
+            {
+                Debug.Log(i + " is odd :(");
+            }
         }
     }
 }
